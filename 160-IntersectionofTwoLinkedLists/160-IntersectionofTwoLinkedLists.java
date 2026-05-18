c6b26da61b7ea999522d56c421be0f85e153138d
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-05-18 12:46:51 +0530

    File updated at 5/18/2026, 12:46:51 PM

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
@@ -1,34 +1,29 @@
-// Last updated: 5/18/2026, 12:34:54 PM
+// Last updated: 5/18/2026, 12:46:51 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode(int x) {
-7 *         val = x;
-8 *         next = null;
-9 *     }
-10 * }
-11 */
-12public class Solution {
-13    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-14        ListNode t1=headA;
-15        ListNode t2=headB;
-16        while(t1!=t2){
-17            if(t1==t2){
-18                return t1;
-19            }
-20            if(t1==null){
-21                t1=headB;
-22            }else{
-23                t1=t1.next;
-24            }
-25            if(t2==null){
-26                t2=headA;
-27            }else{
-28                t2=t2.next;
-29            }
-30        }
-31        return t1;
-32    }
-33}
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode swapPairs(ListNode head) {
+13        ListNode dummy=new ListNode(0);
+14        dummy.next=head;
+15        ListNode curr=dummy;
+16        while(curr.next!=null && curr.next.next!=null){
+17            ListNode first=curr.next;
+18            ListNode second=curr.next.next;
+19            
+20            first.next=second.next;
+21            second.next=first;
+22            curr.next=second;
+23
+24            curr=first;
+25        }
+26        return dummy.next;
+27    }
+28}
