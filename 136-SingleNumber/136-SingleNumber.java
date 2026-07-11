a45db03f24b9a6c09663067f9c55507889eb3978
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-07-11 21:51:45 +0530

    File updated at 7/11/2026, 9:51:46 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,12 +1,14 @@
-// Last updated: 1/20/2026, 9:07:54 PM
+// Last updated: 7/11/2026, 9:51:46 PM
 1class Solution {
 2    public int singleNumber(int[] nums) {
-3        if(nums.length==1) return nums[0];
-4        int n=nums.length;
-5        int ans =0;
-6        for(int i=0;i<n;i++){
-7            ans=ans^nums[i];
-8        }
-9        return ans;
-10    }
-11}
+3     int n=nums.length;
+4     if(n==1){
+5        return nums[0];
+6     }   
+7     int ans=0;
+8     for(int i=0;i<n;i++){
+9        ans=ans^nums[i];
+10     }
+11     return ans;
+12    }
+13}
