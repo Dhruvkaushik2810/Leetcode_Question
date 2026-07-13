25f0bed7df90fafe6cecacef95822e1b8047b485
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-07-13 22:18:54 +0530

    File updated at 7/13/2026, 10:18:53 PM

--- 229-MajorityElementII/229-MajorityElementII.java
+++ 229-MajorityElementII/229-MajorityElementII.java
@@ -1,41 +1,16 @@
-// Last updated: 11/17/2025, 10:19:43 PM
-class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        int cand1 = 0, cand2 = 0;
-        int count1 = 0, count2 = 0;
-        for (int num : nums) {
-            if (num == cand1) {
-                count1++;
-            } 
-            else if (num == cand2) {
-                count2++;
-            } 
-            else if (count1 == 0) {
-                cand1 = num;
-                count1 = 1;
-            } 
-            else if (count2 == 0) {
-                cand2 = num;
-                count2 = 1;
-            } 
-            else {
-                count1--;
-                count2--;
-            }
-        }
-
-        count1 = 0;
-        count2 = 0;
-
-        for (int num : nums) {
-            if (num == cand1) count1++;
-                                                                                                                           else if (num == cand2) count2++;
-        }
-        List<Integer> result = new ArrayList<>();
-        if (count1 > n / 3) result.add(cand1);
-        if (count2 > n / 3) result.add(cand2);
-
-        return result;
-    }
-    }
+// Last updated: 7/13/2026, 10:18:53 PM
+1class Solution {
+2    public List<Integer> majorityElement(int[] nums) {
+3        HashMap<Integer, Integer> map = new HashMap<>();
+4        List<Integer> list = new ArrayList<>();
+5        for(int num:nums){
+6            map.put(num,map.getOrDefault(num,0)+1);
+7        }   
+8        for(int key:map.keySet()){
+9            if(map.get(key)>nums.length/3){
+10                list.add(key);
+11            }
+12        }
+13        return list;
+14    }
+15}
