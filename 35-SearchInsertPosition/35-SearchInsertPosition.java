7033d93a84f57d7ac27fc83f6aa8c94d99f72a59
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-07-27 19:05:32 +0530

    File updated at 7/27/2026, 7:05:33 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,14 +1,21 @@
-// Last updated: 10/26/2025, 10:28:48 PM
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
-            }
-            else if(target<nums[i]){
-                return i;
-            }
-        }
-        return nums.length;
-    }
-}
+// Last updated: 7/27/2026, 7:05:33 PM
+1class Solution {
+2    public int searchInsert(int[] nums, int target) {
+3        int low=0;
+4        int high=nums.length-1;
+5        int mid=0;
+6        while(low<=high){
+7            mid=low+(high-low)/2;
+8            if(nums[mid]==target){
+9                return mid;
+10            }
+11            else if(nums[mid]<target){
+12                low=mid+1;
+13            }
+14            else{
+15                high=mid-1;
+16            }
+17        }
+18        return low;
+19    }
+20}
