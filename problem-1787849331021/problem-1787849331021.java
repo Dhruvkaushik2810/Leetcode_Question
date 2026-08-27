7889f333d5039c9231ed10d77b8a421586458a19
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-08-27 22:18:52 +0530

    Create

---
+++ problem-1787849331021/problem-1787849331021.java
@@ -0,0 +1,10 @@
+// Last updated: 8/27/2026, 10:18:51 PM
+1class Solution {
+2    public int maximumProduct(int[] nums) {
+3        Arrays.sort(nums);
+4        int n=nums.length;
+5        int  left=nums[0]*nums[1]*nums[n-1];
+6        int  right=nums[n-1]*nums[n-2]*nums[n-3];
+7        return Math.max(left,right);
+8    }
+9}
