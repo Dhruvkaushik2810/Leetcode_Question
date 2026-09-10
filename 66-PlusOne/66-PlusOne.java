3cff9ee427d6518c1a38bc25c5abb27c41dc1b94
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-09-10 23:06:21 +0530

    File updated at 9/10/2026, 11:06:20 PM

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,25 +1,21 @@
-// Last updated: 9/10/2026, 11:02:58 PM
+// Last updated: 9/10/2026, 11:06:20 PM
 1class Solution {
 2    public int[] plusOne(int[] digits) {
 3        int n=digits.length;
 4        ArrayList<Integer> nums = new ArrayList<>();
-5        int sum=digits[digits.length-1]+1;
-6        int temp=sum%10;
-7        nums.add(temp);
-8        sum=sum/10;
-9        int carry=sum;
-10        for(int i=n-2;i>=0;i--){
-11            sum=digits[i]+carry;
-12            temp=sum%10;
-13            nums.add(temp);
-14            sum=sum/10;
-15            carry=sum;
-16        }
-17        if(carry==1){
-18            nums.add(carry);
-19        }
-20        Collections.reverse(nums);
-21        int[] result = nums.stream().mapToInt(Integer::intValue).toArray();
-22        return result;
-23    }
-24}
+5        int carry=1;
+6        for(int i=n-1;i>=0;i--){
+7            int sum=digits[i]+carry;
+8            int temp=sum%10;
+9            nums.add(temp);
+10            sum=sum/10;
+11            carry=sum;
+12        }
+13        if(carry==1){
+14            nums.add(carry);
+15        }
+16        Collections.reverse(nums);
+17        int[] result = nums.stream().mapToInt(Integer::intValue).toArray();
+18        return result;
+19    }
+20}
