cc1474636fe7cc93ee3173704aaf34d7b9a89479
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-09-25 12:32:04 +0530

    File updated at 9/25/2026, 12:32:04 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,14 +1,16 @@
-// Last updated: 9/16/2026, 4:45:35 PM
+// Last updated: 9/25/2026, 12:32:04 PM
 1class Solution {
-2    public int maximumGap(int[] nums) {
-3        int gap=0;
-4        Arrays.sort(nums);
-5        for(int i=0;i<nums.length-1;i++){
-6            int diff=nums[i+1]-nums[i];
-7            if(gap<diff){
-8                gap=diff;
-9            }
-10        }
-11        return gap;
-12    }
-13}
+2    public int findMin(int[] nums) {
+3        int low=0;
+4        int high=nums.length-1;
+5        while(low<high){
+6            int mid=low+(high-low)/2;
+7            if(nums[mid]>nums[high]){
+8                low=mid+1;
+9            }else{
+10                high=mid;
+11            }
+12        }
+13        return nums[low];
+14    }
+15}
